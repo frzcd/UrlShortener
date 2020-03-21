d373d422251095866aa37b054439c2b1bdafad2a
Nikitinskiy Daniil <frzcd2@yandex.ru>
2020-03-22 01:59:15 +0300

    commit7

--- src/main/java/com/frzcd/UrlShortener/controller/MainController.java
+++ src/main/java/com/frzcd/UrlShortener/controller/MainController.java
@@ -2,6 +2,8 @@ package com.frzcd.UrlShortener.controller;
 
 import com.frzcd.UrlShortener.dao.Dao;
 import org.apache.commons.validator.routines.UrlValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import java.util.Map;
 @RequestMapping("u")
 public class MainController {
 
+    private static Logger logger = LoggerFactory.getLogger(MainController.class);
+
     Dao dao;
     UrlValidator urlValidator;
 
@@ -23,12 +27,13 @@ public class MainController {
     @GetMapping(path = "/{link}")
     public RedirectView redirect(@PathVariable String link) {
         if (link.length() != 6) {
+            logger.info("Failed to redirect. Short url's length is wrong");
             return new RedirectView("/");
         }
         if (dao.checkIsShortExists(link) != 0) {
             return new RedirectView(dao.getLongByShort(link));
         }
-
+        logger.info("Failed to redirect. Short url is not exist");
         return new RedirectView("/");
     }
 
@@ -36,6 +41,7 @@ public class MainController {
     public HashMap<String, String> generateShortUrl(@RequestBody Map<String, String> request) {
 
         if (!urlValidator.isValid(request.get("text"))) {
+            logger.info("Invalid url received.");
             return new HashMap<String, String>() {{ put("message", "Invalid url received"); }};
         }
         if (dao.checkIsLongExists(request.get("text")) != 0) {
@@ -48,10 +54,10 @@ public class MainController {
     public Map<String, String> getLongUrlByShort(@RequestBody Map<String, String> request) {
         String url = request.get("text")
                 .replaceFirst(host + "u/", "");
-        System.out.println(url);
         if (dao.checkIsShortExists(url) != 0) {
             return new HashMap<String, String>() {{ put("message", dao.getLongByShort(url)); }};
         }
+        logger.info("Failed to return long url by short. Short url is not exists.");
         return new HashMap<String, String>() {{ put("message", "Short url is not exists"); }};
     }
 
